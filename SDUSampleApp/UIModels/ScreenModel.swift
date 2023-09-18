95529c6c93160e5cce6a2b9560c8a8d2ff629141
pawsharm5 <pawsharm5@publicisgroupe.net>
2023-09-18 13:56:08 +0530

    Model Changes

--- SDUSampleApp/UIModels/ScreenModel.swift
+++ SDUSampleApp/UIModels/ScreenModel.swift
@@ -7,22 +7,44 @@
 
 import Foundation
 
+// MARK: - ScreenModel
 struct ScreenModel: Decodable {
-    let pageTitle: String
-    let components: [ComponentModel]
+    let screen: String?
+    let title: ScreenTitle?
+    let elements: [ScreenElement]?
 }
- 
-struct ComponentModel: Decodable {
+
+// MARK: - Element
+struct ScreenElement: Decodable {
     let type: ComponentType?
-    let style: StyleAtrributeModel?
+    let text: String?
+    let style: ScreenElementStyle
+    let placeholder, key: String?
+    let secure: Bool?
+    let action: String?
 }
+
 enum ComponentType: String, Decodable {
     case lable
     case textfield
     case button
 }
-struct StyleAtrributeModel: Decodable {
-    let textColor: String?
+
+// MARK: - ElementStyle
+struct ScreenElementStyle: Decodable {
+    let color: String?
     let fontSize: Int?
+    let backgroundColor: String?
 }
 
+// MARK: - Title
+struct ScreenTitle: Decodable {
+    let text: String?
+    let style: ScreenTitleStyle
+}
+
+// MARK: - TitleStyle
+struct ScreenTitleStyle: Decodable {
+    let color: String?
+    let fontSize: Int?
+}
