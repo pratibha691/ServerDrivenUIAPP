4d6b8fa83434ef0f5b2d0f10de7b7863c0ab4a3b
Pratibha <pratibha.gupta@publicissapient.com>
2023-09-21 11:04:55 +0530

    pod init

---
+++ Pods/Target Support Files/Pods-SDUSampleApp/Pods-SDUSampleApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SDUSampleApp : NSObject
+@end
+@implementation PodsDummy_Pods_SDUSampleApp
+@end
---
+++ Pods/Target Support Files/Pods-SDUSampleAppTests/Pods-SDUSampleAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SDUSampleAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_SDUSampleAppTests
+@end
